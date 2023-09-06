2b3de8882536c142652212dbc33637b60137bbdb
Cristian Aton <cristianeduardo0414@hotmail.com>
2023-09-06 05:57:59 -0400

    Add functionality to buttons
    *displays the number corresponding to the button pressed
    *evaluates a single pair of numbers with the chosen operator
    *adds functionality to the clear all and delete buttons

--- script.js
+++ script.js
@@ -1,48 +1,101 @@
 const btnContainer = document.getElementById("buttons-container");
-const numBtns = document.querySelectorAll(".cal-nums")
+const numBtns = document.querySelectorAll(".cal-nums");
 const screenNum = document.getElementById("num");
+const operatorBtns = document.querySelectorAll(".operator-btn");
+const result = document.getElementById("result");
+const clearBtn = document.getElementById("clear");
+const deleteBtn = document.getElementById("delete");
 
-let num1 = 0;
-let operator = 0;
-let num2 = 0;
+let number1 = "";
+let operator = "";
+let number2 = "";
 
 
-numBtns.forEach(el => el.addEventListener("click", (event) => {
-    let cow1 = screenNum.innerText = event.target.innerText;
-    num1 = event.target.innerText;
+
+numBtns.forEach(button => button.addEventListener("click", () => {
+    if(operator === "") {
+        appendNumber(button.textContent)
+    } else {
+        appendNumber2(button.textContent)
+    }
+}));
+
+operatorBtns.forEach(button => button.addEventListener("click", () => {
+    number1 = screenNum.textContent;
+    operator = button.textContent;
     
-    
-}))
+}));
+
+result.addEventListener("click", function() {
+    let convert1 = +number1;
+    let convert2 = +number2;
+    operate(convert1, convert2);
+})
 
 
-
-
-
-function add(number1, number2) {
-    return number1 + number2;
+function appendNumber(number) {
+    if(screenNum.textContent === "0") {
+      screenNum.textContent = "";
+    }
+    screenNum.textContent += number;
+    number1 = screenNum.textContent;
 }
 
-function sub(num, num2) {
-    return num - num2;
-}
-
-function multi(num, num2) {
-    return num * num2;
-}
-
-function divide(num, num2) {
-    return num / num2;
+function appendNumber2(number) {
+    if(screenNum.textContent === number1) {
+        screenNum.textContent = "";
+      }
+    screenNum.textContent += number
+    number2 = screenNum.textContent
 }
 
 
+clearBtn.addEventListener("click", () => {
+    screenNum.textContent = 0;
+    number1 = "";
+    operator = "";
+    number2 = "";
+})
 
-function operate() {
-    let number1 = parseInt(prompt("first num here"));
-    let operator = prompt("ope");
-    let number2 = parseInt(prompt("sec"));
-    
+deleteBtn.addEventListener("click", () => {
+    if(screenNum.textContent === number1) {
+        number1 = number1.slice(0, -1);
+       screenNum.textContent = number1
+    } else if(screenNum.textContent === number2) {
+        number2 = number2.slice(0, -1);
+        screenNum.textContent = number2
+    }
+ })
 
-    if(operator == "add") {
-        alert(add(number1, number2));
+
+function operate(num1, num2) {
+    if(operator === "+") {
+        add(num1, num2)
+    } else if (operator === "-") {
+        subtract(num1, num2)
+    }  else if (operator === "x") {
+        multiply(num1, num2)
+    }   else if (operator === "÷") {
+        divide(num1, num2)
     }
 }
+
+
+function add(a, b) {
+    screenNum.textContent = a + b;
+}
+
+function subtract(a, b) {
+    screenNum.textContent = a - b;
+}
+
+function multiply(a, b) {
+    screenNum.textContent = a * b;
+}
+
+function divide(a, b) {
+    screenNum.textContent = a / b;
+}
+
+
+
