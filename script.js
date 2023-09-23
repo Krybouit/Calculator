e002ca40fb449b20883bb855ba4a478b6baf00d9
Cristian Aton <cristianeduardo0414@hotmail.com>
2023-09-22 20:04:29 -0400

    Limit decimal input and round decimal numbers
    * limits decimal point to only one on screen.
    
    * rounds answers with long decimals so that they don't overflow
     the screen.
    
    * if the user tries to divide by 0, it returns an error message.
    
    * fixes calculator screen so it doesn't overflow the screen.

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const operatorBtns = document.querySelectorAll(".operator-btn");
 const result = document.getElementById("result");
 const clearBtn = document.getElementById("clear");
 const deleteBtn = document.getElementById("delete");
+const decimal = document.getElementById("decimal");
+let decimalAllowed = true;
 
 let number1 = "";
 let operator = "";
@@ -20,26 +22,36 @@ numBtns.forEach(button => button.addEventListener("click", () => {
     }
 }));
 
+
+operatorBtns.forEach(element => {
+    element.addEventListener("click", function buttonFocus() {
+        document.querySelector(".focus")?.classList.remove("focus");
+        element.classList.add("focus");
+    })
+})
+
+
 operatorBtns.forEach(button => button.addEventListener("click", () => {
-   let convert = +number1;
-   let convert2 = +number2;
+    let convert = +number1;
+    let convert2 = +number2;
      if (!number1 == "" && !number2 == "") {
         operate(convert, convert2)
     } else if (!holdResult == "" && !number2 == "") {
         convert = +holdResult
         operate(convert, convert2)
     }
-    operator = button.textContent;
+    if (!number1 == "" || !holdResult == "") {
+        decimalAllowed = true
+        operator = button.textContent;
+    }
 }));
 
+
 function appendNumber(number) {
-    if(operator == "" && number1 == "") {
-        screenNum.textContent = "";
-    } else if(screenNum.textContent == 0) {
+    if(operator == "" && number1 == "" || screenNum.textContent == 0) {
         screenNum.textContent = "";
     }
-    screenNum.textContent += number;
-    number1 = screenNum.textContent;
+    number1 = screenNum.textContent += number
 }
 
 
@@ -52,14 +64,33 @@ function appendNumber2(number) {
 }
 
 
+decimal.addEventListener("click", () => {
+    if (screenNum.textContent == 0) {
+        screenNum.textContent = "";
+    }
+    if(decimalAllowed == true && operator == "") {
+       number1 = screenNum.textContent += decimal.textContent
+        decimalAllowed = false
+    } else if(decimalAllowed == true && !operator == "") {
+        number2 = screenNum.textContent += decimal.textContent
+        decimalAllowed = false
+    }
+})
+
+
 result.addEventListener("click", function() {
      convert = +number1;
      convert2 = +number2;
-     if(!holdResult == "" && number1 == ""){
+     if(!holdResult == "" && number1 == "" && !number2 == ""){
         convert = +holdResult
+        operate(convert, convert2);
+        operator = "";
+        document.querySelector(".focus")?.classList.remove("focus");
+     } else if (!number1 == "" && !number2 == "") {
+        operate(convert, convert2);
+        operator = "";
+        document.querySelector(".focus")?.classList.remove("focus");
      }
-    operate(convert, convert2);
-    operator = "";
 })
 
 
@@ -70,6 +101,7 @@ clearBtn.addEventListener("click", () => {
     number2 = "";
     holdResult = "";
 })
+
 
 deleteBtn.addEventListener("click", () => {
     if(screenNum.textContent === number1) {
@@ -96,32 +128,38 @@ function operate(num1, num2) {
 
 
 function add(a, b) {
-    screenNum.textContent = a + b;
+    screenNum.textContent = Math.round((a + b) * 100) / 100;
     holdResult = screenNum.textContent;
     number1 = "";
     number2 = "";
 }
+
 
 function subtract(a, b) {
-    screenNum.textContent = a - b;
+    screenNum.textContent = Math.round((a - b) * 100) / 100;
     holdResult = screenNum.textContent;
     number1 = "";
     number2 = "";
 }
+
 
 function multiply(a, b) {
-    screenNum.textContent = a * b;
+    screenNum.textContent = Math.round((a * b) * 100) / 100;
     holdResult = screenNum.textContent;
     number1 = "";
     number2 = "";
 }
+
 
 function divide(a, b) {
-    screenNum.textContent = a / b;
-    holdResult = screenNum.textContent;
-    number1 = "";
-    number2 = "";
+    if(a === 0 && b === 0) {
+        screenNum.textContent = "ERROR"
+        number1 = "";
+        number2 = "";
+    } else {
+        screenNum.textContent = Math.round((a / b) * 100) / 100;
+        holdResult = screenNum.textContent;
+        number1 = "";
+        number2 = "";
+    }
 }
-
-
-
